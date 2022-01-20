5d7aa381c40faed8b6aa08dd28b6572d262bb8be
Peter Droogmans (attiks) <peter@attiks.com>
2022-01-20 18:03:57 +0100

    feat(assessments): Tab for assessments on operation

--- html/modules/custom/hr_paragraphs/src/Controller/ParagraphController.php
+++ html/modules/custom/hr_paragraphs/src/Controller/ParagraphController.php
@@ -25,9 +25,9 @@ class ParagraphController extends ControllerBase {
   }
 
   /**
-   * Check if offices is enabled.
+   * Helper to check if tab is active.
    */
-  public function hasOffices($group) {
+  protected function tabIsActive($group, $tab) {
     if (is_numeric($group)) {
       $group = $this->entityTypeManager->getStorage('group')->load($group);
     }
@@ -37,7 +37,21 @@ class ParagraphController extends ControllerBase {
       $item = $item['value'];
     });
 
-    return AccessResult::allowedIf(in_array('offices', $enabled_tabs));
+    return AccessResult::allowedIf(in_array($tab, $enabled_tabs));
+  }
+
+  /**
+   * Check if offices is enabled.
+   */
+  public function hasOffices($group) {
+    return $this->tabIsActive($group, 'offices');
+  }
+
+  /**
+   * Check if assessments is enabled.
+   */
+  public function hasAssessments($group) {
+    return $this->tabIsActive($group, 'assessments');
   }
 
   /**
@@ -63,4 +77,30 @@ class ParagraphController extends ControllerBase {
     return $view_builder->viewMultiple($offices, $view_mode);
   }
 
+  /**
+   * Return all assessments of an operation, sector or cluster.
+   */
+  public function getAssessments($group) {
+    if ($group->field_operation->isEmpty()) {
+      return array(
+        '#type' => 'markup',
+        '#markup' => $this->t('Operation not set.'),
+      );
+    }
+
+    $operation_uuid = $group->field_operation->entity->uuid();
+
+    $entity_id = 'assessment';
+    $view_mode = 'teaser';
+
+    $assessment_uuids = $this->entityTypeManager->getStorage($entity_id)->getQuery()
+      ->condition('operations', $operation_uuid)
+      ->range(0, 25)
+      ->sort('changed', 'DESC')
+      ->execute();
+    $assessments = $this->entityTypeManager->getStorage($entity_id)->loadMultiple($assessment_uuids);
+
+    $view_builder = $this->entityTypeManager->getViewBuilder($entity_id);
+    return $view_builder->viewMultiple($assessments, $view_mode);
+  }
 }
