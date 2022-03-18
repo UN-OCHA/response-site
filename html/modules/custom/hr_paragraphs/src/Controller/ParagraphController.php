b950b0e7d5bed06a6770bd567ab62bfc8bc91d87
Chris Ruppel <ruppel@reliefweb.int>
2022-03-18 12:51:20 +0100

    feat: a new View Mode for any content displayed as an Operation tab

--- html/modules/custom/hr_paragraphs/src/Controller/ParagraphController.php
+++ html/modules/custom/hr_paragraphs/src/Controller/ParagraphController.php
@@ -212,7 +212,7 @@ class ParagraphController extends ControllerBase {
     }
 
     $entity_type = 'node';
-    $view_mode = 'full';
+    $view_mode = 'operation_tab';
     $params = $link->getUrl()->getRouteParameters();
 
     $office_page = $this->entityTypeManager->getStorage($entity_type)->load($params[$entity_type]);
