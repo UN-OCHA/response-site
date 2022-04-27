a2ba2a639d4b382221a512fdc15a88f356da53a2
Peter Droogmans (attiks) <peter@attiks.com>
2022-04-27 11:54:07 +0200

    test: html and page title

---
+++ html/modules/custom/hr_paragraphs/tests/src/ExistingSite/HrParagraphsOperationTabTitlesTest.php
@@ -0,0 +1,74 @@
+<?php
+
+// phpcs:ignoreFile
+
+namespace Drupal\Tests\hr_paragraphs\ExistingSite;
+
+use Drupal\Core\Url;
+use Drupal\group\Entity\Group;
+use Drupal\node\Entity\Node;
+use weitzman\DrupalTestTraits\ExistingSiteBase;
+
+/**
+ * Tests RSS feed.
+ */
+class HrParagraphsOperationTabTitlesTest extends ExistingSiteBase {
+
+  /**
+   * Test empty operation.
+   */
+  public function testOperationTabTitles() {
+    $group_title = 'My operation';
+
+    $contacts = Node::create([
+      'type' => 'page',
+      'title' => 'My contacts',
+    ]);
+    $contacts->setPublished()->save();
+
+    $pages = Node::create([
+      'type' => 'page',
+      'title' => 'My pages',
+    ]);
+    $pages->setPublished()->save();
+
+    $group = Group::create([
+      'type' => 'operation',
+      'label' => $group_title,
+    ]);
+
+    $group->set('field_ical_url', 'https://www.example.com/events');
+    $group->set('field_offices_page', 'internal:' . $contacts->toUrl()->toString());
+    $group->set('field_pages_page', 'internal:' . $pages->toUrl()->toString());
+    $group->set('field_reliefweb_documents', 'https://reliefweb.int/updates?view=reports');
+    $group->set('field_maps_infographics_link', 'https://reliefweb.int/updates?view=maps');
+    $group->set('field_hdx_dataset_link', 'https://data.humdata.org/dataset');
+    $group->set('field_reliefweb_assessments', 'https://reliefweb.int/updates?advanced-search=%28F5%29&view=reports');
+    $group->setPublished()->save();
+
+    $this->drupalGet($group->toUrl());
+    $this->assertSession()->titleEquals($group_title . ' | ReliefWeb Operations');
+    $this->assertSession()->elementTextEquals('css', 'h1.cd-page-title', $group_title);
+
+    $tabs = [
+      'events' => 'Events',
+      'contacts' => 'Contacts',
+      'pages' => 'Pages',
+      'reports' => 'Reports',
+      'maps' => 'Maps / Infographics',
+      'datasets' => 'Data',
+      'assessments' => 'Assessments',
+    ];
+
+    foreach ($tabs as $tab => $title) {
+      $url = Url::fromRoute('hr_paragraphs.operation.' . $tab, [
+        'group' => $group->id(),
+      ]);
+
+      $this->drupalGet($url);
+      $this->assertSession()->titleEquals($group_title . ' - ' . $title . ' | ReliefWeb Operations');
+      $this->assertSession()->elementTextEquals('css', 'h1.cd-page-title', $title);
+    }
+  }
+
+}
