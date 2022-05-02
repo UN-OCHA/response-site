45e922a3e6a35e942afde4999f85a13a22fc7ce1
Chris Ruppel <ruppel@reliefweb.int>
2022-05-02 16:11:17 +0200

    fix(events): allow keyboard nav to open dialogs for each event
    Refs: HRINFO-1240

--- html/modules/custom/hr_paragraphs/component/events/js/fullcalendar.js
+++ html/modules/custom/hr_paragraphs/component/events/js/fullcalendar.js
@@ -59,8 +59,35 @@
         });
       },
       eventRender: function (event, element, view) {
-        // Add tabindex=0 to allow keyboard to focus on each event.
+        // Allow keyboard to focus on each event.
         element.attr('tabindex', '0');
+
+        // When [Enter] is pressed on focused event, open the modal dialog.
+        element.keypress(function(ev){
+          // Check if the [Enter] key was pressed.
+          if (ev.keyCode == 13) {
+            // Prepare event metadata
+            var dateFormat = 'DD.MM.YYYY hh:mmA';
+            var startdate = event.start.format(dateFormat);
+            var enddate = startdate;
+
+            if (event.end != null) {
+              enddate = event.end.format(dateFormat);
+            }
+
+            // Populate the modal dialog with this event's metadata.
+            $('#modalID').html(event.id);
+            $('#modalDescription').html(event.description);
+            $('#modalLocation').html(event.location);
+            $('#modalStartDate').html(startdate);
+            $('#modalEndDate').html(enddate);
+
+            // Display the modal
+            $('#fullCalModal').dialog({
+              title: event.title,
+            });
+          }
+        })
       },
     });
 
