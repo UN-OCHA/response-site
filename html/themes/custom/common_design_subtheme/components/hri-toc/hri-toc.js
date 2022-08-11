2f07b24a480208cbb913a4d2ea2b82ded6dac865
Chris Ruppel <ruppel@reliefweb.int>
2022-08-11 11:04:22 +0200

    fix: apply smooth-scrolling unless user prefers reduced motion
    Refs: RWR-185

--- html/themes/custom/common_design_subtheme/components/hri-toc/hri-toc.js
+++ html/themes/custom/common_design_subtheme/components/hri-toc/hri-toc.js
@@ -4,29 +4,49 @@
 (function () {
   'use strict';
 
-  const toc = [];
-  const paragraphs = document.querySelectorAll('.field--name-field-paragraphs > .field__item > *');
-  const target = document.querySelector('.hri-toc__list');
-  let output = '';
+  Drupal.behaviors.tableOfContents = {
+    attach: function (context, settings) {
+      // Initialize with a few variables.
+      const toc = [];
+      const paragraphs = document.querySelectorAll('.field--name-field-paragraphs > .field__item > *');
+      const target = document.querySelector('.hri-toc__list');
+      let output = '';
 
-  // Loop through Paragraphs in the main content column and collect essential
-  // info about them.
-  paragraphs.forEach(function (el) {
-    var paragraphTitle = el.querySelector('.field--name-field-title') && el.querySelector('.field--name-field-title').innerText;
+      // Loop through Paragraphs in the main content column and collect essential
+      // info about them.
+      paragraphs.forEach(function (el) {
+        var paragraphTitle = el.querySelector('.field--name-field-title') && el.querySelector('.field--name-field-title').innerText;
 
-    if (paragraphTitle !== null) {
-      toc.push({
-        id: el.id,
-        title: paragraphTitle
+        if (paragraphTitle !== null) {
+          toc.push({
+            id: el.id,
+            title: paragraphTitle
+          });
+        }
       });
+
+      // For all items found, construct some HTML.
+      toc.forEach(function (item) {
+        output += '<li><a href="#' + item.id + '">' + item.title + '</a></li>';
+      });
+
+      // Append to DOM
+      target.innerHTML = output;
+
+      // Set up smooth-scrolling for ToC.
+      //
+      // First, check for prefers-reduced-motion and only continue if the media
+      // query resolves to false.
+      const tocLinks = document.querySelectorAll('.hri-toc__list a');
+      if (window.matchMedia('(prefers-reduced-motion: reduce)').matches === false) {
+        tocLinks.forEach(link => {
+          link.addEventListener('click', function (ev) {
+            ev.preventDefault();
+            var target = '#' + link.getAttribute('href').split('#')[1];
+            document.querySelector(target).scrollIntoView({behavior: 'smooth'});
+          });
+        });
+      }
     }
-  });
-
-  // For all items found, construct some HTML.
-  toc.forEach(function (item) {
-    output += '<li><a href="#' + item.id + '">' + item.title + '</a></li>';
-  });
-
-  // Append to DOM
-  target.innerHTML = output;
+  };
 })();
