b45517d867b6e79885b7e5c537a959c518745ecb
Peter Droogmans (attiks) <peter@attiks.com>
2023-02-16 16:07:03 +0100

    bug: Select blocked users
    Refs: #RWR-326

---
+++ html/modules/custom/hr_paragraphs/src/Plugin/Group/RelationHandler/GroupMembershipAllEntityReference.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\hr_paragraphs\Plugin\Group\RelationHandler;
+
+use Drupal\group\Plugin\Group\RelationHandler\GroupMembershipEntityReference;
+use Drupal\Core\Field\BaseFieldDefinition;
+
+/**
+ * Configures the entity reference for the group_membership relation plugin.
+ */
+class GroupMembershipAllEntityReference extends GroupMembershipEntityReference {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function configureField(BaseFieldDefinition $entity_reference) {
+    $this->parent->configureField($entity_reference);
+
+    $handler_settings = $entity_reference->getSetting('handler_settings');
+    $handler_settings['include_anonymous'] = FALSE;
+
+    $entity_reference->setSetting('handler', 'default:all_users');
+    $entity_reference->setSetting('handler_settings', $handler_settings);
+  }
+
+}
