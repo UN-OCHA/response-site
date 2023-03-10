4f55ca900d5eaf5c7e7b8521906a9f5877b7d3ba
Peter Droogmans (attiks) <peter@attiks.com>
2023-03-10 12:37:04 +0100

    feat: login destination
    Refs: #RWR-328

--- html/themes/custom/common_design_subtheme/js/login_destination.js
+++ html/themes/custom/common_design_subtheme/js/login_destination.js
@@ -4,7 +4,7 @@
  */
 
 (function () {
-  var loginLink = document.querySelector('a[href="/user/login/hid"]');
+  var loginLink = document.querySelector('a[href$="/user/login/hid"]');
   if (loginLink) {
     loginLink.href += '?destination=' + location.pathname + location.search + location.hash;
   }
