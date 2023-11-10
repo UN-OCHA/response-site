c5b44367938881808bf3cc79474ca72df26ded99
Peter Droogmans (attiks) <peter@attiks.com>
2023-11-10 16:54:05 +0100

    POC
    Refs: #OPS-9765

---
+++ html/modules/custom/ocha_drd/src/Plugin/monitoring/SensorPlugin/OchaDeploymentIdentifierSensorPlugin.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\ocha_drd\Plugin\monitoring\SensorPlugin;
+
+use Drupal\monitoring\Result\SensorResultInterface;
+use Drupal\monitoring\SensorPlugin\SensorPluginBase;
+
+/**
+ * Monitors deployment identifier.
+ *
+ * @SensorPlugin(
+ *   id = "ocha_deployment_identifier",
+ *   label = @Translation("Deployment identifier"),
+ *   description = @Translation("Deployment identifier."),
+ *   addable = FALSE
+ * )
+ *
+ * Based on the drupal core system state cron_last.
+ */
+class OchaDeploymentIdentifierSensorPlugin extends SensorPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function runSensor(SensorResultInterface $result) {
+    $deployment_identifier = \Drupal\Core\Site\Settings::get('deployment_identifier');
+    $result->setValue($deployment_identifier ?? 'not specified');
+    $result->setMessage($deployment_identifier ?? 'Not specified');
+
+    if ($deployment_identifier) {
+      $result->setStatus(SensorResultInterface::STATUS_OK);
+    }
+    else {
+      $result->setStatus(SensorResultInterface::STATUS_UNKNOWN);
+    }
+  }
+
+}
