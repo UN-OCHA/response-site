f1ea03e6f72d7665bd856fd162fc9f110f3a00b2
Peter Droogmans (attiks) <peter@attiks.com>
2024-10-11 16:08:45 +0200

    chore: Code clean up

--- html/modules/custom/rwr_sitrep/rwr_sitrep.post_update.php
+++ html/modules/custom/rwr_sitrep/rwr_sitrep.post_update.php
@@ -2,13 +2,13 @@
 
 /**
  * @file
- * Install hooks.
+ * Post update hooks hooks.
  */
 
 /**
  * Add default taxonomy term for cluster_or_working_group_type.
  */
-function rwr_sitrep_install() {
+function rwr_sitrep_post_update_add_terms() {
   // Add default terms.
   $term = rwr_sitrep_create_term_if_needed('cluster_or_working_group_type', 'Situation Report');
   $term->set('field_pdf_enabled', TRUE);
